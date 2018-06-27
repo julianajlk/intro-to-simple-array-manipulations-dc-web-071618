155e973a61f2f45893f5b33c72ad95334998907f
Juliana Kang <julianajlk@gmail.com>
2018-06-27 13:44:04 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  countries_in_western_africa.push
+  countries_in_western_africa.push("next_country")
   countries_in_western_africa
 end
